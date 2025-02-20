c982c3499dc08000f8b3b57028509115b6c7940f
beris147 <edwir147@gmail.com>
2025-02-20 15:22:30 -0800

    better auth user management

--- src/app/(auth)/utils.ts
+++ src/app/(auth)/utils.ts
@@ -1,7 +1,23 @@
+'use server';
+
 import { createClient } from '@/utils/supabase/server';
+import { AuthError, User } from '@supabase/supabase-js';
+
+interface AuthUserResponse {
+	user: User | null;
+	error: AuthError | null;
+}
+
+export async function getAuthUser(): Promise<AuthUserResponse> {
+	const supabase = await createClient();
+	const { data, error } = await supabase.auth.getUser();
+	return {
+		user: data.user,
+		error: error,
+	};
+}
 
 export async function isUserLoggedIn(): Promise<boolean> {
-	const supabase = await createClient();
-	const { data } = await supabase.auth.getUser();
-	return data?.user !== null;
+	const authUser = await getAuthUser();
+	return authUser?.user !== null;
 }
--- src/app/private/page.tsx
+++ src/app/private/page.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import { redirect } from 'next/navigation';
-
-import { createClient } from '@/utils/supabase/server';
+import { getAuthUser } from '../(auth)/utils';
 
 export default async function PrivatePage() {
-	const supabase = await createClient();
-
-	const { data, error } = await supabase.auth.getUser();
-	if (error || !data?.user) {
+	const { user, error } = await getAuthUser();
+	if (error || !user) {
 		redirect('/login');
 	}
-
-	return <p>Hello {data.user.email}</p>;
+	return <p>Hello {user.email}</p>;
 }
